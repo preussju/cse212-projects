6c6278c82a794842114292c8dad07b55cf419da1
preussju <jveiga@byupathway.edu>
2026-03-14 09:29:47 -0300

    part 2

--- week02/code/PriorityQueue.cs
+++ week02/code/PriorityQueue.cs
@@ -24,14 +24,16 @@
 
         // Find the index of the item with the highest priority to remove
         var highPriorityIndex = 0;
-        for (int index = 1; index < _queue.Count - 1; index++)
+        for (int index = 1; index <= _queue.Count - 1; index++)  // added the <= to make sure it goes until the end of the list
         {
             if (_queue[index].Priority >= _queue[highPriorityIndex].Priority)
                 highPriorityIndex = index;
         }
 
-        // Remove and return the item with the highest priority
+        // Remove and return the item with the highest priority 
         var value = _queue[highPriorityIndex].Value;
+        _queue.RemoveAt(highPriorityIndex); //removed the item 
+
         return value;
     }
 
--- week02/code/PriorityQueue_Tests.cs
+++ week02/code/PriorityQueue_Tests.cs
@@ -6,23 +6,49 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 public class PriorityQueueTests
 {
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: get the highest priority item from the queue
+    // Expected Result: Will my priority queue return the item with the highest priority first? and then on?
+    // Defect(s) Found: it was not removing the high priority item; it was not going until the end of the list.
     public void TestPriorityQueue_1()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+
+        priorityQueue.Enqueue("Test 1 should be last", 1);
+        priorityQueue.Enqueue("Test 2 should be first", 5);
+        priorityQueue.Enqueue("Test 3 should be second", 3);
+
+        var highPriority = priorityQueue.Dequeue(); // Should return "Test 2 should be first"
+        var mediumPriority = priorityQueue.Dequeue(); // Should return "Test 3 should be second"
+
+        // Assert.Fail("Implement the test case and then remove this.");
+        if (highPriority != "Test 2 should be first")
+            Assert.Fail("Failed: " + highPriority);
+        else
+            Assert.IsTrue(true);
+        if (mediumPriority != "Test 3 should be second")
+            Assert.Fail("Failed: " + mediumPriority);
+        else
+            Assert.IsTrue(true);
+
     }
 
     [TestMethod]
-    // Scenario: 
-    // Expected Result: 
-    // Defect(s) Found: 
+    // Scenario: try to dequeue from an empty queue
+    // Expected Result: if the list is empty and I try to dequeue, does it throw an exception with the appropriate error message?
+    // Defect(s) Found: none :)
     public void TestPriorityQueue_2()
     {
         var priorityQueue = new PriorityQueue();
-        Assert.Fail("Implement the test case and then remove this.");
+
+        try
+        {
+            var emptyList = priorityQueue.Dequeue(); // empty list
+            Assert.Fail("Exception should have been thrown.");
+        }
+        catch (InvalidOperationException e)
+        {
+            Assert.AreEqual("The queue is empty.", e.Message);
+        }
     }
 
     // Add more test cases as needed below.
