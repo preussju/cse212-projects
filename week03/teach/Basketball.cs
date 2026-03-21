2af6e652e1445d901b63bbfefd0ddefcfb6b1b2e
preussju <jveiga@byupathway.edu>
2026-03-21 09:14:12 -0300

    team activities w3

--- week03/teach/Basketball.cs
+++ week03/teach/Basketball.cs
@@ -27,10 +27,25 @@ public class Basketball
             var fields = reader.ReadFields()!;
             var playerId = fields[0];
             var points = int.Parse(fields[8]);
+
+            if (players.ContainsKey(playerId)) {
+                players[playerId] += points;
+            } else {
+                players[playerId] = points;
+            }
         }
 
         Console.WriteLine($"Players: {{{string.Join(", ", players)}}}");
 
         var topPlayers = new string[10];
+
+        var array = players.ToArray();
+        Array.Sort(array);
+
+        for (var i = 0; i < 10; ++i)
+        {
+            Console.WriteLine(topPlayers[i]);
+        }
+
     }
 }
